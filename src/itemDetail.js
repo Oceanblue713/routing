8c52e131f92f9af11ad9e71726ffa04535fdac76
Anri Liy <amedamachip713@gmail.com>
2020-04-24 16:12:22 -0700

    Complete ItemDetails

--- src/itemDetail.js
+++ src/itemDetail.js
@@ -1,19 +1,29 @@
 import React, {useState, useEffect}from 'react';
 import './App.css';
-import {Link} from 'react-router-dom';
 
-function Item() {
+
+function ItemDetail({ match }) {
 
   useEffect(() => {
+    fetchItem();
   }, []);
 
-  const [item, setItem] = useState({});
+  const [item, setItem] = useState({
+    images: {}
+  });
+
+  const fetchItem = async() => {
+    const fetchItem = await fetch(`?ids={match.params.id}`);
+    const item = await fetchItem.json();
+    setItem(item);
+  }
 
   return (
     <div>
-      <h1>Item</h1>
+      <h1>{item.name}</h1>
+      <img src={item.image} />
     </div>
   );
 }
 
-export default Item;
+export default ItemDetail;
